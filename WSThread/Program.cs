a8cc30cdba7df5b0154c97c371cda38cf44be5c6
Duffaut <maxime.duffaut1@orange.fr>
2019-05-29 09:38:34 +0200

    2nd part Lambda Expression done

--- WSThread/Program.cs
+++ WSThread/Program.cs
@@ -7,22 +7,17 @@ using System.Threading.Tasks;
 namespace WSThread
 {
 
-    delegate int DelegateAddNumbers(int x, int y);
+    delegate int DelegateSquareUp(int x);
 
     class Program
     {
         static void Main(string[] args)
         {
-            DelegateAddNumbers handler = AddNumbers;
-            int res = handler(10, 15);
-            int res2 = handler.Invoke(20, 22);
-            Console.WriteLine("Result is : {0}, {1}", res, res2);
+            DelegateSquareUp squaUp = new DelegateSquareUp(x => x * x);
+            int res = squaUp.Invoke(4);
+            Console.WriteLine("Result is : {0}", res);
             Console.Read();
         }
-
-        static int AddNumbers(int x, int y)
-        {
-            return x + y;
-        }
+        //static Func<int, int> SquareUp = x => x * x;
     }
 }
