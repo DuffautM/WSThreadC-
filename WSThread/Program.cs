ccb2808bd2cab35e657b204eb94a72f4ff381a57
Duffaut <maxime.duffaut1@orange.fr>
2019-05-29 09:09:32 +0200

    1st part

---
+++ WSThread/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSThread
+{
+
+    delegate int DelegateAddNumbers(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateAddNumbers handler = AddNumbers;
+            int res = handler(10, 15);
+            int res2 = handler.Invoke(20, 22);
+            Console.WriteLine("Result is : {0}, {1}", res, res2);
+            Console.Read();
+        }
+
+        static int AddNumbers(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
