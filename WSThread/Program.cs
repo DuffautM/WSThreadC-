ae69d97d4c6654f3505949fd677bcfaf3db50b9e
Duffaut <maxime.duffaut1@orange.fr>
2019-05-29 12:18:34 +0200

    11th part done

--- WSThread/Program.cs
+++ WSThread/Program.cs
@@ -9,35 +9,81 @@ namespace WSThread
     class Program
     {
         private delegate void DELG(object state);
-        private static int var = 0;
-        private static object _Lock = new object ();
-        static Semaphore semaphore;
+        private delegate void callback(IAsyncResult async);
+        private static System.Threading.ReaderWriterLockSlim rwls;
+        private static int val = 0;
 
         static void Main(string[] args)
         {
-            semaphore = new Semaphore(2, 2);
-            DELG d = (state) =>
+            rwls = new System.Threading.ReaderWriterLockSlim();
+            DELG Read1 = (state) =>
             {
-                string name_thread = (string)state;
-                semaphore.WaitOne();
-                for(int i = 0; i<3; i++)
+                string thread = (string)state;
+                while (true)
                 {
-                    ++var;
-                    Console.WriteLine("Thread -> {0} -- var -> {1}", name_thread, var.ToString());
+                    rwls.EnterReadLock();
+                    try
+                    {
+                        Console.WriteLine("Thread -> {0} -- Message -> {1}", thread, val.ToString());
+                    }
+                    finally
+                    {
+                        rwls.ExitReadLock();
+                    }
                     System.Threading.Thread.Sleep(2000);
                 }
-                semaphore.Release();
-
             };
 
-            Thread t1 = new Thread(d.Invoke);
-            Thread t2 = new Thread(d.Invoke);
-            Thread t3 = new Thread(d.Invoke);
-            t1.Start(((object)("T1")));
-            t2.Start(((object)("T2")));
-            t3.Start(((object)("T3")));
-            Console.Read();
 
+            DELG Write1 = (state) =>
+            {
+                string thread = (string)state;
+                int[] tb = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+                for (int i = 0; i < 10; i++)
+                {
+                    rwls.EnterWriteLock();
+                    try
+                    {
+                        val = tb[i];
+                        Console.WriteLine("Changement de val par Thread -> {0}", thread);
+                    }
+                    finally
+                    {
+                        rwls.ExitWriteLock();
+                    }
+                    System.Threading.Thread.Sleep(3000);
+                }
+            };
+            DELG Write2 = (state) =>
+            {
+                string thread = (string)state;
+                int[] tb = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+                for (int i = 0; i < 10; i++)
+                {
+                    rwls.EnterWriteLock();
+                    try
+                    {
+                        val = tb[i];
+                        Console.WriteLine("Changement de val par Thread -> {0}", thread);
+                    }
+                    finally
+                    {
+                        rwls.ExitWriteLock();
+                    }
+                    System.Threading.Thread.Sleep(3000);
+                }
+            };
+
+            System.Threading.Thread t1 = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(Read1.Invoke));
+            System.Threading.Thread t2 = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(Read1.Invoke));
+            System.Threading.Thread t3 = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(Write1.Invoke));
+            System.Threading.Thread t4 = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(Write1.Invoke));
+            t1.Start((object)("T1"));
+            t2.Start((object)("T2"));
+            t3.Start((object)("T3"));
+            t4.Start((object)("T4"));
+            Console.Read();
         }
     }
+
 }
