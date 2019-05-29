3f917c4e11249622131e06a4a45320923c6ad365
Duffaut <maxime.duffaut1@orange.fr>
2019-05-29 12:05:18 +0200

    10th part done

--- WSThread/Program.cs
+++ WSThread/Program.cs
@@ -11,29 +11,31 @@ namespace WSThread
         private delegate void DELG(object state);
         private static int var = 0;
         private static object _Lock = new object ();
-        static Mutex mutex;
+        static Semaphore semaphore;
 
         static void Main(string[] args)
         {
-            mutex = new Mutex(false);
+            semaphore = new Semaphore(2, 2);
             DELG d = (state) =>
             {
                 string name_thread = (string)state;
-                mutex.WaitOne();
+                semaphore.WaitOne();
                 for(int i = 0; i<3; i++)
                 {
                     ++var;
                     Console.WriteLine("Thread -> {0} -- var -> {1}", name_thread, var.ToString());
                     System.Threading.Thread.Sleep(2000);
                 }
-                mutex.ReleaseMutex();
+                semaphore.Release();
 
             };
 
             Thread t1 = new Thread(d.Invoke);
             Thread t2 = new Thread(d.Invoke);
+            Thread t3 = new Thread(d.Invoke);
             t1.Start(((object)("T1")));
             t2.Start(((object)("T2")));
+            t3.Start(((object)("T3")));
             Console.Read();
 
         }
